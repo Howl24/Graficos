7c2ebb4f42b0a6e2b67c9cf63b46684505928622
Daniel Matos <daniel.matos@pucp.pe>
2017-12-06 12:24:18 -0500

    wippp

--- ta/foo.cpp
+++ ta/foo.cpp
@@ -1,6 +1,7 @@
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
+//
 // (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
@@ -123,6 +124,10 @@ typedef struct Mesh{
     Vertex center;
     float scale;
 
+    bool intersect(const Vec3f &rayorig, const Vec3f &raydir, float &t0, float &t1) const{
+        return true;
+    }
+
     //Matriz de transformación
     //glm::mat4 model_transform;
 
@@ -135,7 +140,7 @@ typedef struct Mesh{
     //GLuint ibo_object;
 }Mesh;
 
-Mesh* leerOFF(const char* filename){
+Mesh leerOFF(const char* filename){
     FILE* fid = fopen(filename, "rt");
 
     //Leer formato
@@ -203,7 +208,7 @@ Mesh* leerOFF(const char* filename){
     mesh->scale = 2.0/diag;
 
     //mesh->model_transform = glm::mat4(1.0f);
-    return mesh;
+    return *mesh;
 }
 
 //[comment]
@@ -217,64 +222,176 @@ float mix(const float &a, const float &b, const float &mix)
 }
 
 
-// Pov ray
+//Vec3f trace(
+//    const Vec3f &rayorig,
+//    const Vec3f &raydir,
+//    const Mesh* &mesh,
+//    const int &depth)
+//{
+//    float tnear = INFINITY;
+//    const Mesh* mesh = NULL;
+//
+//    for (unsigned i = 0; i< mesh->numTriangles; ++i){
+//        float t0 = INFINITY, t1 = INFINITY;
+//        if (mesh->triangles[i].intersect(rayorig, raydir, t0, t1)){
+//            if (t0 < 0) t0 = t1;
+//            if (t0 < tnear) {
+//                tnear = t0;
+//                triangle = mesh->triangles[i];
+//            }
+//        }
+//    }
+//
+//
+//    float t0 = INFINITY, t1 = INFINITY;
+////    if (mesh.
+//}
+//
 
 Vec3f trace(
     const Vec3f &rayorig,
     const Vec3f &raydir,
-    const Mesh* &mesh,
+    const std::vector<Mesh> &scene,
     const int &depth)
 {
     float tnear = INFINITY;
     const Mesh* mesh = NULL;
-
-    for (unsigned i = 0; i< mesh->numTriangles; ++i){
+    // find intersection of this ray with the sphere in the scene
+    for (unsigned i = 0; i < scene.size(); ++i){
         float t0 = INFINITY, t1 = INFINITY;
-        if (mesh->triangles[i].intersect(rayorig, raydir, t0, t1)){
+        if ( scene[i].intersect(rayorig, raydir, t0, t1)){
             if (t0 < 0) t0 = t1;
             if (t0 < tnear) {
                 tnear = t0;
-                triangle = mesh->triangles[i];
+                mesh = &scene[i];
             }
         }
     }
+    // if there's no intersection return black or background color
+    //if (#!sphere) return Vec3f(2);
+    //else return Vec3f(3);
+    //Vec3f surfaceColor = 0; // color of the ray/surfaceof the object intersected by the ray
+    //Vec3f phit = rayorig + raydir * tnear; // point of intersection
+    //Vec3f nhit = phit - sphere->center; // normal at the intersection point
+    //nhit.normalize(); // normalize normal direction
+    //// If the normal and the view direction are not opposite to each other
+    //// reverse the normal direction. That also means we are inside the sphere so set
+    //// the inside bool to true. Finally reverse the sign of IdotN which we want
+    //// positive.
+    //float bias = 1e-4; // add some bias to the point from which we will be tracing
+    //bool inside = false;
+    //if (raydir.dot(nhit) > 0) nhit = -nhit, inside = true;
+    //if ((sphere->transparency > 0 || sphere->reflection > 0) && depth < MAX_RAY_DEPTH) {
+    //    float facingratio = -raydir.dot(nhit);
+    //    // change the mix value to tweak the effect
+    //    float fresneleffect = mix(pow(1 - facingratio, 3), 1, 0.1);
+    //    // compute reflection direction (not need to normalize because all vectors
+    //    // are already normalized)
+    //    Vec3f refldir = raydir - nhit * 2 * raydir.dot(nhit);
+    //    refldir.normalize();
+    //    Vec3f reflection = trace(phit + nhit * bias, refldir, spheres, depth + 1);
+    //    Vec3f refraction = 0;
+    //    // if the sphere is also transparent compute refraction ray (transmission)
+    //    if (sphere->transparency) {
+    //        float ior = 1.1, eta = (inside) ? ior : 1 / ior; // are we inside or outside the surface?
+    //        float cosi = -nhit.dot(raydir);
+    //        float k = 1 - eta * eta * (1 - cosi * cosi);
+    //        Vec3f refrdir = raydir * eta + nhit * (eta *  cosi - sqrt(k));
+    //        refrdir.normalize();
+    //        refraction = trace(phit - nhit * bias, refrdir, spheres, depth + 1);
+    //    }
+    //    // the result is a mix of reflection and refraction (if the sphere is transparent)
+    //    surfaceColor = (
+    //        reflection * fresneleffect +
+    //        refraction * (1 - fresneleffect) * sphere->transparency) * sphere->surfaceColor;
+    //}
+    //else {
+    //    // it's a diffuse object, no need to raytrace any further
+    //    for (unsigned i = 0; i < spheres.size(); ++i) {
+    //        if (spheres[i].emissionColor.x > 0) {
+    //            // this is a light
+    //            Vec3f transmission = 1;
+    //            Vec3f lightDirection = spheres[i].center - phit;
+    //            lightDirection.normalize();
+    //            for (unsigned j = 0; j < spheres.size(); ++j) {
+    //                if (i != j) {
+    //                    float t0, t1;
+    //                    if (spheres[j].intersect(phit + nhit * bias, lightDirection, t0, t1)) {
+    //                        transmission = 0;
+    //                        break;
+    //                    }
+    //                }
+    //            }
+    //            surfaceColor += sphere->surfaceColor * transmission *
+    //            std::max(float(0), nhit.dot(lightDirection)) * spheres[i].emissionColor;
+    //        }
+    //    }
+    //}
 
-
-    float t0 = INFINITY, t1 = INFINITY;
-    if (mesh.
-
-
+    //return surfaceColor + sphere->emissionColor;
 }
 
 
-//Render function
-void render(Mesh* mesh){
-    unsigned width = 640, height=480;
+
+
+void render(const std::vector<Mesh> &scene)
+{
+    unsigned width = 640, height = 480;
     Vec3f *image = new Vec3f[width * height], *pixel = image;
     float invWidth = 1 / float(width), invHeight = 1 / float(height);
     float fov = 30, aspectratio = width / float(height);
     float angle = tan(M_PI * 0.5 * fov / 180.);
 
-    for (unsigned y = 0; y < height; ++y) {
-        for (unsigned x = 0; x < width; ++x, ++pixel){
+    for (unsigned y = 0; y < height; ++y){
+        for (unsigned x = 0 ; x< width; ++x, ++ pixel) {
             float xx = (2 * ((x + 0.5) * invWidth) - 1) * angle * aspectratio;
             float yy = (1 - 2 * ((y + 0.5) * invHeight)) * angle;
             Vec3f raydir(xx, yy, -1);
             raydir.normalize();
-            *pixel = trace(Vec3f(0), raydir, mesh, 0);
+            *pixel = trace(Vec3f(0), raydir, scene, 0); 
         }
     }
 
     std::ofstream ofs("./untitled.ppm", std::ios::out | std::ios::binary);
     ofs << "P6\n" << width << " " << height << "\n255\n";
-    for (unsigned i = 0; i < width * height; ++i) {
-        ofs << (unsigned char)(std::min(float(1), image[i].x) * 255) <<
-               (unsigned char)(std::min(float(1), image[i].y) * 255) <<
-               (unsigned char)(std::min(float(1), image[i].z) * 255);
+    for (unsigned i = 0; i < width*height; ++ i){
+        ofs << (unsigned char)(std::min(float(1), image[i].x)*255) <<
+               (unsigned char)(std::min(float(1), image[i].y)*255) <<
+               (unsigned char)(std::min(float(1), image[i].z)*255);
     }
     ofs.close();
-    delete [] image;
+    delete[] image;
 }
+
+
+//Render function
+//void render(Mesh* mesh){
+//    unsigned width = 640, height=480;
+//    Vec3f *image = new Vec3f[width * height], *pixel = image;
+//    float invWidth = 1 / float(width), invHeight = 1 / float(height);
+//    float fov = 30, aspectratio = width / float(height);
+//    float angle = tan(M_PI * 0.5 * fov / 180.);
+//
+//    for (unsigned y = 0; y < height; ++y) {
+//        for (unsigned x = 0; x < width; ++x, ++pixel){
+//            float xx = (2 * ((x + 0.5) * invWidth) - 1) * angle * aspectratio;
+//            float yy = (1 - 2 * ((y + 0.5) * invHeight)) * angle;
+//            Vec3f raydir(xx, yy, -1);
+//            raydir.normalize();
+//            *pixel = trace(Vec3f(0), raydir, mesh, 0);
+//        }
+//    }
+//
+//    std::ofstream ofs("./untitled.ppm", std::ios::out | std::ios::binary);
+//    ofs << "P6\n" << width << " " << height << "\n255\n";
+//    for (unsigned i = 0; i < width * height; ++i) {
+//        ofs << (unsigned char)(std::min(float(1), image[i].x) * 255) <<
+//               (unsigned char)(std::min(float(1), image[i].y) * 255) <<
+//               (unsigned char)(std::min(float(1), image[i].z) * 255);
+//    }
+//    ofs.close();
+//    delete [] image;
+//}
 
 //[comment]
 // In the main function, we will create the scene which is composed of 5 spheres
@@ -283,21 +400,13 @@ void render(Mesh* mesh){
 //[/comment]
 int main(int argc, char **argv)
 {
-    Mesh* mesh = leerOFF("aviones.cpp");
-    //render(mesh);
-
-    //srand48(13);
-    std::vector<Sphere> spheres;
-    // position, radius, surface color, reflectivity, transparency, emission color
-    spheres.push_back(Sphere(Vec3f( 0.0, -10004, -20), 10000, Vec3f(0.20, 0.20, 0.20), 0, 0.0));
-    //spheres.push_back(Sphere(Vec3f( 0.0,      0, -20),     4, Vec3f(1.00, 0.32, 0.36), 1, 0.5));
-    //spheres.push_back(Sphere(Vec3f( 5.0,     -1, -15),     2, Vec3f(0.90, 0.76, 0.46), 1, 0.0));
-    //spheres.push_back(Sphere(Vec3f( 5.0,      0, -25),     3, Vec3f(0.65, 0.77, 0.97), 1, 0.0));
-    //spheres.push_back(Sphere(Vec3f(-5.5,      0, -15),     3, Vec3f(0.90, 0.90, 0.90), 1, 0.0));
-    //// light
-    //spheres.push_back(Sphere(Vec3f( 0.0,     20, -30),     3, Vec3f(0.00, 0.00, 0.00), 0, 0.0, Vec3f(3)));
-    render(spheres);
-
+    printf("Hello\n");
+    Mesh mesh = leerOFF("avion.off");
+    printf("Mesh loaded\n");
+    std::vector<Mesh> scene;
+    scene.push_back(mesh);
+    printf("Rendering scene\n");
+    render(scene);
     return 0;
 }
 
